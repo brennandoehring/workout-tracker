789ee3cc8d578951a56f535fe0799598b22ee7f0
brennandoehring <nannerbdale@gmail.com>
2021-01-24 09:42:16 -0500

    Added export and POST route

---
+++ routes/api-routes.js
@@ -0,0 +1,14 @@
+const app = require("express").Router();
+const Workout = require("../models/workout.js");
+
+// POST new workout
+app.post("/api/workout", ({ body }, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+    console.log("Added new workout!");
+});
