9f98071673f7d6cabe6afcb0b0900ac4f3d6115a
brennandoehring <nannerbdale@gmail.com>
2021-01-24 09:33:17 -0500

    Created routes files, added routes to server js, added workout model

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 8000;
 
@@ -9,11 +10,16 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(morgan(dev));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
      useNewUrlParser: true,
      useUnifiedTopology: true,
     });
+
+// Routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
 // Listen
 app.listen(PORT, () => {
