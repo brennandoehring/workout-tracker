85d5da41ae5394d6907db48b90de3e038f97de72
brennandoehring <nannerbdale@gmail.com>
2021-01-24 10:35:30 -0500

    Added put and get routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,4 +13,44 @@ app.post("/api/workout", ({ body }, res) => {
     console.log("Added new workout!");
 });
 
+// GET last workout
+app.get("/api/workout", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+    console.log("Got last workout!");
+});
+
+// PUT an exercise in 
+app.put("/api/workout/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: {exercises: req.body}},
+        { new: true}
+    )
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+    console.log("Added an exercise!");
+});
+
+// GETs workouts for the graphs and charts
+app.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+    console.log("Displaying visuals!");
+});
+
 module.exports = app;
--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(morgan("dev"));
-app.use(express.static("public")); 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
      useNewUrlParser: true,
